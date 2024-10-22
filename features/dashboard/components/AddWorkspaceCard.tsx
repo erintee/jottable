c7661ae645f061acaa52ee6814214fe852d945a6
Erin Templeton <erindtempleton@gmail.com>
2024-10-21 18:52:55 -0700

    Add input fields and styling to new workspace card

--- features/dashboard/components/AddWorkspaceCard.tsx
+++ features/dashboard/components/AddWorkspaceCard.tsx
@@ -10,8 +10,8 @@ import {
   const WorkspaceCard: React.FC = () => {
     return (
       <Card className='p-6 text-center'>
-        <CardTitle>New Workspace</CardTitle>
-        <CardDescription className='italic'>Add a new workspace</CardDescription>
+        <input placeholder='New Workspace' className='placeholder:text-2xl placeholder:text-muted-foreground text-black text-center text-2xl font-semibold leading-tight focus:outline-black'/>
+        <input placeholder='Add a description'className='text-muted-foreground text-center italic text-sm focus:outline-black'/>
         <CardContent className='pt-4 pb-0'>
             +
         </CardContent>
